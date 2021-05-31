48996c8cc02206846fa9d851b16ff0e7df6c213f
Safa-M <mohamedsafa61@gmail.com>
2021-05-31 12:02:51 +0100

    up date

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./MovieCard.css";
-import { Link } from "react-router-dom";
 
 const MovieCard = ({ newCard }) => {
   return (
@@ -17,11 +16,7 @@ const MovieCard = ({ newCard }) => {
         <div className="info">
           <h3>{newCard.title}</h3>
           <p>{newCard.description}</p>
-          <button>
-            <Link to={`/${newCard.title}`} className="see-trailer">
-              SEE TRAILER
-            </Link>
-          </button>
+          <button>SEE TRAILER</button>
         </div>
         <img src={newCard.poster} alt={newCard.title} />
         {newCard.children}
--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -2,21 +2,25 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 import MovieCard from "./MovieCard/MovieCard";
+import { Link } from "react-router-dom";
 
 const MovieList = ({ movieslist, searchValue, rating }) => {
   return (
     <>
-      {movieslist
-        .filter(
-          (el) =>
-            el.title.toLowerCase().includes(searchValue.toLowerCase().trim()) &&
-            el.rating >= rating
-        )
-        .map((newCard, i) => (
-          <div key={i}>
-            <MovieCard newCard={newCard} />
-          </div>
-        ))}
+      
+        {movieslist
+          .filter(
+            (el) =>
+              el.title
+                .toLowerCase()
+                .includes(searchValue.toLowerCase().trim()) &&
+              el.rating >= rating
+          )
+          .map((newCard, i) => (
+            <div key={i}>
+             <Link to={`/${newCard.title}`} className="see-trailer"><MovieCard newCard={newCard} /></Link> 
+            </div>
+          ))}
     </>
   );
 };
